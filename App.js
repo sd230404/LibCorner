812ad5e9fd9646f7d404a602dc54f52cfe324875
mohitkumar2107 <mohit.kumar1@wissen.com>
2024-07-17 11:30:00 +0530

    edited app.js message

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>what's upppp!</Text>
+      <Text>This is my landing page!</Text>
       <StatusBar style="auto" />
     </View>
   );
